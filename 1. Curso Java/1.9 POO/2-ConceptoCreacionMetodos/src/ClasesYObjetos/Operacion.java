681c42c0266b9e527c44a3560192161b6370d2dc
MattHenao <70972324+MattHenao@users.noreply.github.com>
2022-10-03 07:23:25 -0500

    POO

---
+++ 1. Curso Java/1.9 POO/2-ConceptoCreacionMetodos/src/ClasesYObjetos/Operacion.java
@@ -0,0 +1,53 @@
+/*
+ * Aqui veremos la creacion de metodos en las clases
+ */
+package ClasesYObjetos;
+
+import javax.swing.JOptionPane;
+
+
+public class Operacion {
+    //Atributos
+    int numero1;
+    int numero2;
+    int suma;
+    int resta;
+    int multiplicacion;
+    int division;
+    
+    //Metodos
+    
+    //Metodo para pedir al usuario que nos digite dos numeros
+    public void leerNumeros(){
+        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer numero: "));
+        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo numero: "));
+    }
+    
+    //Metodo para sumar ambos numeros
+    public void sumar(){
+        suma = numero1 + numero2;
+    }
+    
+    //Metodo para restar ambos numeros
+    public void restar(){
+        resta = numero1 - numero2;
+    }
+    
+    //Metodo para multiplicar ambos numeros
+    public void multiplicar(){
+        multiplicacion = numero1 * numero2;
+    }
+    
+    //Metodo para dividir ambos numeros
+    public void dividir(){
+        division = numero1 / numero2;
+    }
+    
+    //Mostrar resultados
+    public void mostrarResultados(){
+        System.out.println("La suma es " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicacion es: " + multiplicacion);
+        System.out.println("La division es: " + division);
+    }
+}
